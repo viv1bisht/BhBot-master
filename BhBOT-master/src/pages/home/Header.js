986dc120838aa3fea9fc25279b1a8d2ddc51df83
viv1bisht <vivekbisht1088@gmail.com>
2023-12-07 18:11:26 +0530

    Update Header.js

--- BhBOT-master/src/pages/home/Header.js
+++ BhBOT-master/src/pages/home/Header.js
@@ -1,15 +1,21 @@
 
-import bhbbot_logo from "../../assets/images/bhbbot_logo.png";
+import Logo_bot from "../../assets/images/Logo_bot.png";
 import GoogleSignIn from "../../components/auth/GoogleSignIn";
-
+import LoginSignup from "../../components/auth/LoginSignup";
+// import{
+//   BrowserRouter,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
 const Header = () => {
   return (
     <div>
       <header>
         <div className="logo">
-        <img src={bhbbot_logo} alt="Bharat-Bot" />
+        <img src={Logo_bot} alt="Bharat-Bot" />
         </div>
         <GoogleSignIn />
+        <LoginSignup />
       </header>
     </div>
   );
